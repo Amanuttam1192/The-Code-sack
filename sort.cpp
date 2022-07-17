e3f785e86b16417663d2b8d061ac5c3e811e0cd2
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-07-17 12:15:12 +0530

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long;
+#define ai calling int;
+#define ddls double;
+int main(){
+	void generate(int n){
+		int magic square[n],[n];
+	    int slac;
+	    for(i=0;i<slac;--i){
+	    	slac[i];
+		}
+	    int t;
+	    while(t--){
+	    	sort.slac(a,a++){
+	    		
+			}
+	    slac[]{
+		int j;
+		
+		}
+		for(i=0;i<slac;--i)
+			for(int j=0;j<slac;--i) 
+		
+			
+	    	
+		}
+	}
+}
