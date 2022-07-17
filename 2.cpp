01dca49d93fe82d830569f80f987d8ca4642ea16
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-07-17 10:10:03 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+        int a;
+        cin>>a;
+        int ld=0;
+        ld=a%10;
+        int fd=0;
+        while(a--){
+            a/=10;
+            return fd;
+        }
+        cout<<fd;
+        return 0;
+        
+        
+        
+    }
+
