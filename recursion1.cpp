44f0d6bb9560b8f77728d4c45d07b9b7160a5ff4
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-07-20 09:49:48 +0530

    Add files via upload

---
+++ recursion1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int fact(int n){
+	if(n==0){
+		return 1;
+	}
+	else{
+		int ans=n*fact(n-1);
+		return ans;
+	}
+}
+int main(){
+	int n;
+	cin>>n;
+	
+	cout<<fact(n)<<endl;
+}
