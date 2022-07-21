693b33d1a8f24a1e1ea6f7075992ef5b58468980
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-07-21 10:40:15 +0530

    Add files via upload

---
+++ recursion5.cpp
@@ -0,0 +1,15 @@
+#include<bits/sdtc++.h>
+#include<iostream>
+using namespace std;
+int print_1(int n){
+	if(n==0) return 0;
+	else if(n==1) return 1;
+	else{
+		int ans=return 
+	}
+}
+int  main(){
+	int n;
+	cin>>n;
+	cout<<print_(n)<<endl;
+}
