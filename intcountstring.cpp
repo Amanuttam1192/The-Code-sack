8b261637da1f4691205dd83e5fcf76d4db9e4adc
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-07-30 09:45:17 +0530

    Create intcountstring.cpp

---
+++ intcountstring.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+bool isNumber(string s)
+{
+	for (int i = 0; i < s.length(); i++)
+		if (isdigit(s[i]) == false)
+			return false;
+
+	return true;
+}
+
+int main()
+{
+	string str = "6790";
+
+	if (isNumber(str))
+		cout << "Integer";
+
+	else
+		cout << "String";
+}
