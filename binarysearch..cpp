2fa59c07ff4bb4333e83e42ed7e3b599c923265e
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-08-14 12:52:45 +0530

    Add files via upload

--- binarysearch..cpp
+++ binarysearch..cpp
@@ -4,17 +4,19 @@ int binsearch(int n,int arr[],int x){
 	sort(arr,arr+n);
 	int low=0;
 	int high=n-1;
+	int ans=0;
 	while(low<=high){
 		int mid=(low+high)/2;
-		if(arr[mid]==x) return mid;
-		else if(x<arr[mid]){
-			high=mid-1;
-		}
+		if(arr[mid]>=x) {
+		ans=arr[mid];
+		high=mid-1;
+	}
+	
 		else if(x>arr[mid]){
 		    low=mid+1;
 		}
 	}
-	return -1;
+	return ans;
 	
 };
 int main(){
