6d4a73096f037e87c840fb8d650686933a77ba05
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-08-19 23:25:18 +0530

    Create even length binary.cpp

---
+++ even length binary.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countSeq(int n, int diff)
+{
+	if (abs(diff) > n)
+		return 0;
+
+	if (n == 1 && diff == 0)
+		return 2;
+	if (n == 1 && abs(diff) == 1)
+		return 1;
+
+	int res = 
+			countSeq(n-1, diff+1) +
+
+			2*countSeq(n-1, diff) +
+
+			countSeq(n-1, diff-1);
+
+	return res;
+}
+
+int main()
+{
+	int n = 2;
+	cout << "Count of sequences is "
+		<< countSeq(2, 0);
+	return 0;
+}
