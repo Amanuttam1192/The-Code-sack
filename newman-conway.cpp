8466aefe902eb584343ba95bfb69a3b25c78ad64
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-10-15 21:50:02 +0530

    Create newman-conway.cpp

---
+++ newman-conway.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+//gfg code
+void sequence(int n)
+{
+	int f[n + 1];
+	f[0] = 0;
+	f[1] = 1;
+	f[2] = 1;
+	
+	cout << f[1] << " " << f[2] << " ";
+	
+	for (int i = 3; i <= n; i++) {
+		f[i] = f[f[i - 1]] + f[i - f[i - 1]];	
+		cout << f[i] << " ";
+	}
+}
+
+int main()
+{
+	int n = 13;
+	sequence(n);
+	return 0;
+}
